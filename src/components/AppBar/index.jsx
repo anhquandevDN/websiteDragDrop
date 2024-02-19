5f3a786fea010b5a7ab012f8b74f7a1aada0439e
anhquandev <huynh.anh.quan.vn.com@gmail.com>
2024-02-19 12:26:26 +0700

    themeoverride_p2

--- src/components/AppBar/index.jsx
+++ src/components/AppBar/index.jsx
@@ -30,7 +30,7 @@ function AppBar() {
       <Box sx={{ display: 'flex', alignItems: 'center', gap: 2 }}>
         <AppsIcon sx={{ color: 'primary.main' }} />
         <Box sx={{ display: 'flex', alignItems: 'center', gap: 0.5 }}>
-          <SvgIcon component={trelloIcon} inheritViewBox sx={{ color: 'primary.main' }} />
+          <SvgIcon component={trelloIcon} fontSize="small" inheritViewBox sx={{ color: 'primary.main' }} />
           <Typography variant="span" sx={{ fontSize: '1.2rem', fontWeight: 'bold', color: 'primary.main' }}
           >Trello</Typography>
         </Box>
@@ -48,12 +48,12 @@ function AppBar() {
 
         <Tooltip title="Notification">
           <Badge color="secondary" variant="dot" sx={{ cursor: 'pointer' }} >
-            <NotificationsNoneIcon />
+            <NotificationsNoneIcon sx={{ color: 'primary.main' }}/>
           </Badge>
         </Tooltip>
 
         <Tooltip title="Help">
-          <HelpOutlineIcon sx={{ cursor: 'pointer' }} />
+          <HelpOutlineIcon sx={{ cursor: 'pointer', color: 'primary.main' }} />
         </Tooltip>
 
         <Profiles />
