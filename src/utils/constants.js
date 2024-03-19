1e05e370d45fc746fb391c89eb69e946ab5cca63
anhquandev <huynh.anh.quan.vn.com@gmail.com>
2024-03-19 20:51:15 +0700

    fix_env_DEVANDPRO

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,2 +1,12 @@
-//export const API_ROOT = 'http://localhost:8017'
-export const API_ROOT = 'https://draganddropapi.onrender.com'
+let apiRoot = ''
+console.log('import.meta.env: ', import.meta.env)
+console.log('process.env: ', process.env)
+if (process.env.BUILD_MODE === 'dev') {
+  apiRoot = 'http://localhost:8017'
+}
+if (process.env.BUILD_MODE === 'production') {
+  apiRoot = 'https://draganddropapi.onrender.com'
+}
+console.log('🚀 ~ apiRoot:', apiRoot)
+export const API_ROOT = apiRoot
+//export const API_ROOT = 'https://draganddropapi.onrender.com'
--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,10 @@ import svgr from 'vite-plugin-svgr'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  //cho phép thằng vite sử dụng được process.env, mặc định thì không mà phải dùng import.meta.env (https://github.com/vitejs/vite/issues/1973)
+  define: {
+    'process.env': process.env
+  },
   plugins: [
     react(),
     svgr()
